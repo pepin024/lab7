28ba6a776f05e3dba49864e30260ad16e04d41b8
pepin024 <pepin024@umn.edu>
2017-04-19 20:08:15 -0500

    Merged master fixed conflicts

---
+++ colorIndexHead.h
@@ -0,0 +1,27 @@
+/* 
+ * File:   colorIndexHead.h
+ * Author: Justus
+ *
+ * Created on April 19, 2017, 9:12 AM
+ */
+
+#ifndef COLORINDEXHEAD_H
+#define	COLORINDEXHEAD_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+    void setIndex(unsigned char index, char red, char grn, char blu);
+    void setIndexPacked(unsigned char index, long packedCol);
+    unsigned char getR(unsigned char index);
+    unsigned char getG(unsigned char index);
+    unsigned char getB(unsigned char index);
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* COLORINDEXHEAD_H */
+
