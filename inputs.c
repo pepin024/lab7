0bf84fc048d6369b4d40d3c91202184c9c98c708
pepin <pepin@DESKTOP-Q5M0C25.domain>
2017-04-29 18:36:41 -0500

    Added smapling to AN

--- inputs.c
+++ inputs.c
@@ -7,6 +7,8 @@
 #include <libpic30.h>
 #include "inputs.h"
 
+#define NUMOFSAMPLES 5
+
 void inputInit(){
     
     TRISBbits.TRISB7 = 1;
@@ -16,6 +18,10 @@ void inputInit(){
     AD1PCFGbits.PCFG1 = 0;
     TRISAbits.TRISA0 = 1;
     TRISAbits.TRISA1 = 1;
+    
+        //pins to control the shape of the eye and cycle through the color:
+    TRISBbits.TRISB8 = 1; //shape of eye?
+    TRISBbits.TRISB10 = 1; //color of eye?
     
     T3CON = 0;
     PR3 = 100;
@@ -43,17 +49,32 @@ void inputInit(){
 
 controllerData scanInputs(){
     controllerData foo;
-    foo.c = PORTBbits.RB7;
-    foo.z = PORTBbits.RB8;
+    int sample = 0;
+    int xJoystickSum = 0;
+    int yJoystickSum = 0;
     
-    _AD1IF = 0;
-    T3CONbits.TON = 1;
-    while(_AD1IF == 0);
-    T3CONbits.TON = 0;
-    TMR3 = 0;
-    _AD1IF = 0;
+    foo.c = !PORTBbits.RB7;
+    foo.z = !PORTBbits.RB8;
     
-    foo.joyX = ADC1BUF0;
-    foo.joyY = ADC1BUF1;
+   //below is to return the average of 5 samples of foo.joyX and foo.joyY:
+    //method 1 : change the AD1CON2bits.SMPI to = 0b1010 to interrupt at every 10 samples. -- NO --I DIDNT USE THIS... IT WOULDNT WORK
+    //and implement a loop to read from the adc1buffer:
+    //NUMOFSAMPLES is #def'd up top
+     for(sample = 0; sample < NUMOFSAMPLES; sample++) {//buf0 (x) then buf1 (y))
+        _AD1IF = 0;
+        T3CONbits.TON = 1;
+        while(_AD1IF == 0);
+        T3CONbits.TON = 0;
+        TMR3 = 0;
+        _AD1IF = 0;
+        xJoystickSum = xJoystickSum + ADC1BUF0;
+        yJoystickSum = yJoystickSum + ADC1BUF1;
+    }
+    //now divide the sum by 5 and return it:
+    foo.joyX = xJoystickSum / NUMOFSAMPLES; 
+    foo.joyY = yJoystickSum / NUMOFSAMPLES; 
+    //below is the original code
+    //foo.joyX = ADC1BUF0;
+    //foo.joyY = ADC1BUF1;
  return foo;
 }
