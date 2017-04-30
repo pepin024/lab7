0503cff1f0533cb4142f1545ce35e82a30767345
pepin <pepin@10.104.98.241>
2017-04-30 12:49:23 -0500

    worked on y shift

--- main.c
+++ main.c
@@ -31,10 +31,26 @@
 neopixel leftEye, rightEye; //these represent each LED matrix, they are of the neopixel Struct type defined in the NEOPIX.h
 unsigned long int colorArray[16];
 unsigned char array[] = {4, 2, 1, 15, 15};
-unsigned char blank[] = {0, 0, 0, 0, 0};
 unsigned char test1[] = {0, 1, 2, 3, 4};
 
-int diamond[40]; 
+unsigned char diamond[40] = {   0, 2, 0, 0, 0, 0, 0, 0,
+                                2, 1, 2, 0, 0, 0, 0, 0,
+                                0, 2, 0, 0, 0, 0, 0, 0,
+                                0, 0, 0, 0, 0, 0, 0, 0,
+                                0, 0, 0, 0, 0, 0, 0, 0};
+
+unsigned char square[40] = {    2, 2, 2, 2, 0, 0, 0, 0,
+                                2, 1, 1, 2, 0, 0, 0, 0,
+                                2, 2, 2, 2, 0, 0, 0, 0,
+                                0, 0, 0, 0, 0, 0, 0, 0,
+                                0, 0, 0, 0, 0, 0, 0, 0};
+
+unsigned char blank[40] = {     0, 0, 0, 0, 0, 0, 0, 0,
+                                0, 0, 0, 0, 0, 0, 0, 0,
+                                0, 0, 0, 0, 0, 0, 0, 0,
+                                0, 0, 0, 0, 0, 0, 0, 0,
+                                0, 0, 0, 0, 0, 0, 0, 0};
+
 
 void setup(void) { //initializer function
     CLKDIVbits.RCDIV = 0; // sets clock speed to 16MHz
@@ -45,28 +61,9 @@ void setup(void) { //initializer function
     inputInit();
     initColorArray(colorArray);
     asm("nop");
-    
-    int i = 0;
-    for(i =0; i <40; i++){
-        if(i == 1 || i == 8 || i == 10 || i == 17){
-            diamond[i] = 2;
-        }
-        else if(i == 9) {
-            diamond[i] = 1;
-        }
-        else{
-            diamond[i] = 0;
-        }
-    }
-    
     return;
 }
 
-
-void loop(void) 
-{
-    return;
-}
 
 
 int main(void) {
@@ -87,203 +84,47 @@ int main(void) {
         setIndexPacked(2, 255, 255, 0, yellowEye);
     
     
-    unsigned char array[40];    //holds pattern to be displayed
+    unsigned char displayArray[40];    //holds pattern to be displayed
     setBrightness(&rightEye, 60); //sets the brightness of right and left eye to 60/255
     setBrightness(&leftEye, 60);
     
     controllerData buttons;
-    unsigned char flag = 1; //flag checks if eyes should be rewritten
-    int xPos = 1;
-    int yPos = 2;
-    int colorchoose = 0;
-    int shapechoose = 0;
-    int oldButtonC;
-    int oldButtonZ;
-    int oldX, oldY,botLeftPix; //used to see if the eye has moved.
-    unsigned char eyeCol = 0, oldEyeCol = 0;
     
-    int displayArray[40];
+
    
     
     while (1) 
     {
-        buttons = scanInputs();
+        int xShift, yShift;
         
-        if (buttons.c) {
-            if(oldButtonC == 0){
-                colorchoose++;
-                flag = 1;
-            }
-            if(colorchoose > 2){
-                colorchoose = 0;
-            }
-        }
+        for(yShift = 0; yShift < 8; ++yShift){
         
-        oldButtonC = buttons.c;
-        if(buttons.z){
-            if(oldButtonZ == 0){
-                shapechoose++;
-                flag = 1;
-            }
-            if(shapechoose > 3){
-                shapechoose = 0;
-            }
-        }
-        oldButtonZ = buttons.z;
-        
-        xPos = ((buttons.joyX)-304)/211; //Maps joyX to 0-2
-        if(xPos > 2)
-            xPos = 2; //ensures that xPos doesn't exceed 2
-        xPos = 2 - xPos; // The joy stick reads left as max, so the "2-" maps left to min and right to max
-        yPos = ((buttons.joyY)-322)/123; //Maps joyY to 0-4
-        if(yPos > 4)
-            yPos = 4; //ensures that yPos doesn't exceed 4
-        yPos = 4 - yPos; // The joy stick reads down as max, so the "4-" maps down to min and up to max
-        
-        
-        //this is the regular square version:
-        if(shapechoose == 0){
-            //THIS SECTION WILL ASSEMBLE THE ARRAY - POSITION OF THE EYE
-            if(xPos!=oldX || yPos!=oldY) //checks if the eye has moved from its old position
-            {
-                botLeftPix = xPos*8+yPos; //botLeftPix represents the bottom left position of the iris
-                int i;
-                for(i=0; i<40; i++)
-                {
-                    if((i>=botLeftPix && i<botLeftPix+4) || (i>=botLeftPix+8 && i<botLeftPix+12) || (i>=botLeftPix+16 && i<botLeftPix+20))
-                    { //if statement is used to catch an index that is part of the iris or pupil which should be a color other than black
-                        if(i==botLeftPix+9 || i==botLeftPix+10) //this catches if the index is part of the pupil and sets it white
-                        {
-                            array[i]=1;
-                        }
-                        else    //otherwise the index is part of the iris and is set to the desired color.
-                        {
-                            array[i]=2;
-                        }
-                    }
-                    else //if an index is not part of the iris or pupil then it is sets to black
-                    {
-                        array[i]=0;
-                    }
-                }
-                flag = 1; //set the flag to rewrite to the matrices
-                oldX = xPos; //update old positions
-                oldY = yPos;
-                
-            }
-        }
-//         * THE MATRIX WRITES FROM THE BOTTEM LEFT UP. ONCE IT REACHES THE TOP IT LOOPS BACK TO THE NEXT LED ON THE BOTTOM
-// * 
-//  ----------------
-// | 07 15 23 31 39 |
-// | 06 14 22 30 38 |
-// | 05 13 21 29 37 |
-// | 04 12 20 28 36 |
-// | 03 11 19 27 35 |
-// | 02 10 18 26 34 |
-// | 01 09 17 25 33 |
-// | 00 08 16 24 32 |
-//  ----------------
-        else if (colorchoose == 1) { //diamond 
-            int i = 0;
-            int a =0;
-            //pupil = 1
-            //iris = 2
-            //else = 0;
-            for(i = 0; i < 40; i++){
-                a = (xPos*8)+(i + yPos);
-                if (a > 40) {
-                    a = a - 40;
-                    array[a] = 0;
-                }                    
-                else{
-                //for the iris
-                    array[a] = diamond[i];
+
+            int i, j;
+
+            for(i = 39; i >= 0; --i){
+                j = i - yShift;
+                if(j < 0 || ((j/8) != (i/8))){
+                    displayArray[i] = 0;
+                } else {
+                   displayArray[i] = square[j]; 
                 }
                 
             }
-            
+            sendColor(&leftEye, redEye, displayArray);
+
+            for(i = 39; i >= 0; --i){
+                j = i - yShift;
+                if(j < 0 || ((j/8) != (i/8))){
+                    displayArray[i] = 0;
+                } else {
+                    displayArray[i] = diamond[j];
+                }
+            }
+            sendColor(&rightEye, redEye, displayArray);
+
+            __delay_ms(1000);
         }
-        
-        else if (colorchoose == 2) { //horizontal lines of color
-            int i = 0;
-            for (i = 0; i < 40;i++){
-                array[i] = i / 8; //5 groups 8...vertical stripes.
-            }
-            
-        }
-        
-        
-        if(flag) //if we should rewrite the matrices
-        {
-            flag = 0; //reset flag
-            //use conditions for the correct eye color
-            if(colorchoose == 0)
-            {
-                sendColor(&rightEye, blueEye, array);
-                __delay_ms(5);
-                sendColor(&leftEye, blueEye, array);
-                __delay_ms(5);
-            }
-            else if(colorchoose == 1)
-            {
-                sendColor(&rightEye, redEye, array);
-                __delay_ms(5);
-                sendColor(&leftEye, redEye, array);
-                __delay_ms(5);
-            }
-            else
-            {
-                sendColor(&rightEye, yellowEye, array);
-                __delay_ms(5);
-                sendColor(&leftEye, yellowEye, array);
-                __delay_ms(5);
-            }
-        }
-    }       
+    }
 }
-
-
-
-/* EACH EYE WILL LOOK LIKE THE IMAGE BELOW
- * "." REPRESENTS A BLACK (OFF) LED
- * "=" REPRESENTS THE IRIS WHICH IS A COLOR THAT IS SELECTED
- * "+" REPRESENTS THE PUPIL WHICH WILL BE WHITE IN THIS CASE
- 
-  -----------
- | . . . . . |
- | . . . . . |
- | . = = = . |
- | . = + = . |
- | . = + = . |
- | . = = = . | <-THE LOWER LEFT OF THE IRIS IS WHAT botLeftPix MAPS TO, THIS IS USED AS A REFERENCE POINT TO BUILD THE EYE
- | . . . . . |
- | . . . . . |
-  -----------
- 
- * THE MATRIX WRITES FROM THE BOTTEM LEFT UP. ONCE IT REACHES THE TOP IT LOOPS BACK TO THE NEXT LED ON THE BOTTOM
- * 
-  ----------------
- | 07 15 23 31 39 |
- | 06 14 22 30 38 |
- | 05 13 21 29 37 |
- | 04 12 20 28 36 |
- | 03 11 19 27 35 |
- | 02 10 18 26 34 |
- | 01 09 17 25 33 |
- | 00 08 16 24 32 |
-  ----------------
- */
-
-        /*setBrightness(&rightEye, buttons.joyX >> 2);
-        setBrightness(&leftEye, buttons.joyY >> 2);
-        if(buttons.c)
-            sendColor(&rightEye, colorArray, array);
-        else
-            sendColor(&rightEye, colorArray, test1);
-        __delay_ms(5);
-        if(buttons.z)
-            sendColor(&leftEye, colorArray, array); //sendColor takes 952 cycles between each write color
-        else
-            sendColor(&leftEye, colorArray, blank);
-        __delay_ms(5);*/
+   
